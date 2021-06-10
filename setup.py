77f659dce64abb06ca0cb47f9047a0ad9d1e4476
CarolineMorton <carolinemorton@gmx.com>
2021-06-10 20:23:32 +0100

    update setup with entry points

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-import os
 from setuptools import setup, find_namespace_packages
 
-with open(os.path.join("VERSION")) as f:
-    version = f.read().strip()
+from osmatching.version import __version__
 
 setup(
     name="opensafely-matching",
-    version=version,
+    version=__version__,
     packages=find_namespace_packages(exclude=["tests"]),
     url="https://github.com/opensafely/matching",
+    description="Command line tool for matching cases to controls",
     author="OpenSAFELY",
+    license="GPLv3",
     author_email="tech@opensafely.org",
     python_requires=">=3.7",
     install_requires=["pandas"],
+    entry_points={"console_scripts": ["match=osmatching.__main__:main"]},
     classifiers=["License :: OSI Approved :: GNU General Public License v3 (GPLv3)"],
 )
